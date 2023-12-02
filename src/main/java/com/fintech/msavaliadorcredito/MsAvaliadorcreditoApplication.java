73b9711be9ab36d81b876c8a4684a48f84d88d49
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2023-12-02 15:26:53 -0300

    commit inicial

---
+++ src/main/java/com/fintech/msavaliadorcredito/MsAvaliadorcreditoApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.msavaliadorcredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAvaliadorcreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAvaliadorcreditoApplication.class, args);
+	}
+
+}
